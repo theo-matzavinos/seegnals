a2854c1943cc0c6c7ae9513ea411fdc3c9ba993e
Theophilos Matzavinos <tmatzavinos@singularlogic.eu>
2024-08-26 12:26:14 +0300

    analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,14 @@
 import tutorialkit from "@tutorialkit/astro";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+  output: "server",
   devToolbar: {
     enabled: false,
   },
   integrations: [tutorialkit()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
